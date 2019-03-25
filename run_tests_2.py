2e3a79379bcddb0d5d57cd727070fe4765dc2d93
Sidharth Singla <sidpkl.singla@gmail.com>
2019-03-24 21:50:55 -0400

    Improving run_tests

--- run_tests_2.py
+++ run_tests_2.py
@@ -1,5 +1,12 @@
 import subprocess, time, os
 import pdb
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--num_slaves', type=str, required=True)
+args = parser.parse_args()
+num_slaves = args.num_slaves
+
 prod_env = {'SPARK_HOME': '/home/s6singla/spark-2.4.0-bin-hadoop2.7',
             'JAVA_HOME': '/usr/lib/jvm/java-8-oracle',
             'CONDA_PYTHON_EXE': '/home/s6singla/anaconda3/bin/python',
@@ -18,7 +25,7 @@ master_url_cmd = "--master_url spark://129.97.173.68:7077"
 
 chunks = {'susy': [('180', '36'), ('360', '72')],
           'cats_dogs_small': [('360', '72'), ('720', '144'), ('1080', '216'), ('1440', '288'), ('1800', '360'), ('2160', '432')],
-          'cats_dogs': [('66', '13'), ('132', '26'), ('198', '39')]
+          'cats_dogs': [('340', '68'), ('680', '136'), ('1020', '204')]
             }
 datasets = ['susy', 'cats_dogs_small', 'cats_dogs']
 
@@ -33,21 +40,23 @@ dataset_env['cats_dogs_small'] = {}
 dataset_env['cats_dogs_small']['path'] = 'cats_dogs_small.csv'
 dataset_env['cats_dogs_small']['chunksize'] = '100'
 
-num_slaves = '2'
-
 for dataset in datasets:
     for chunk in chunks[dataset]:
         train_chunks, test_chunks = chunk
 
         sub_dir = dataset + "_" + train_chunks + "_" + test_chunks + "_" + num_slaves
-        path_sub_dir = output_dir + sub_dir
-        #os.mkdir(path_sub_dir)
+        abs_path_sub_dir = output_dir + sub_dir
+        os.mkdir(abs_path_sub_dir)
+        os.chmod(abs_path_sub_dir, 777)
+
+        abs_path_sparkml_json = abs_path_sub_dir + "/" + json_sparkml_file
+        abs_path_sparkling_water_json = abs_path_sub_dir + "/" + json_sparkling_file
 
         cmd = model_all_cmd + " --path_to_csv=" + dataset_env[dataset]['path'] + " --dataset=" + dataset + " --num_train_chunks=" + train_chunks + \
               " --num_test_chunks=" + test_chunks + " --chunksize=" + dataset_env[dataset]['chunksize'] + " " + master_url_cmd
 
-        pyspark_cmd = pyspark_run_cmd + cmd + " --json_log_file=" + path_sub_dir + "/" + json_sparkml_file
-        pysparkling_cmd = pysparkling_run_cmd + cmd + " --json_log_file=" + path_sub_dir + "/" + json_sparkling_file
+        pyspark_cmd = pyspark_run_cmd + cmd + " --json_log_file=" + abs_path_sparkml_json
+        pysparkling_cmd = pysparkling_run_cmd + cmd + " --json_log_file=" + abs_path_sparkling_water_json
 
         pdb.set_trace()
         try:
@@ -68,5 +77,8 @@ for dataset in datasets:
 
         try:
             print( "Generating Graphs" )
-            generate_graphs_cmd = "python3 generate_runtime_comparison_graphs.py " + "--sparkml_json " +
-            subprocess.run()
+            generate_graphs_cmd = "python3 generate_runtime_comparison_graphs.py " + " --sparkml_json " + abs_path_sparkml_json + \
+                                  " --sparkling_water_json " + abs_path_sparkling_water_json + " --output_dir " + abs_path_sub_dir
+            subprocess.run(generate_graphs_cmd, shell=True, check=True, env=prod_env )
+
+        time.sleep(5)
